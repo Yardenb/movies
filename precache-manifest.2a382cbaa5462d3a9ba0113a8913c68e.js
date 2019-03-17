473eb4228b54f010d0dff52a0c060066a7596910
Yardenb <yardenesh@gmail.com>
2019-03-17 11:49:24 +0200

    Updates

---
+++ precache-manifest.2a382cbaa5462d3a9ba0113a8913c68e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f32cded71e3f76f0915a",
+    "url": "/movies/static/css/main.5348493b.chunk.css"
+  },
+  {
+    "revision": "f32cded71e3f76f0915a",
+    "url": "/movies/static/js/main.0e7d7115.chunk.js"
+  },
+  {
+    "revision": "04129ba559ae92aa086e",
+    "url": "/movies/static/js/runtime~main.712e9881.js"
+  },
+  {
+    "revision": "1a5edaac9b5eb6e5c052",
+    "url": "/movies/static/js/2.9a3b21fb.chunk.js"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/movies/static/media/Roboto-Regular.5673da52.ttf"
+  },
+  {
+    "revision": "9745415b8a2a8b9a2480a12f7e2d3ad3",
+    "url": "/movies/static/media/Roboto-Medium.9745415b.ttf"
+  },
+  {
+    "revision": "d5c46b134c17b2138dcd1bb0efa67049",
+    "url": "/movies/static/media/Roboto-Light.d5c46b13.ttf"
+  },
+  {
+    "revision": "e7f249e71a563eed9c495835657eb9c2",
+    "url": "/movies/static/media/PoetsenOne-Regular.e7f249e7.ttf"
+  },
+  {
+    "revision": "eb09afa14652b5ca3c2e5e76be87ef7a",
+    "url": "/movies/static/media/header.eb09afa1.jpg"
+  },
+  {
+    "revision": "f4afb210b6b744ae22a6fc82c8b72001",
+    "url": "/movies/index.html"
+  }
+];
